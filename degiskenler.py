09e822373e214aa914dee25ca18ec5276a0ff7b3
kemalcalak <kemalcalak@gmail.com>
2023-06-02 22:36:26 +0300

    1.commit

---
+++ degiskenler.py
@@ -0,0 +1,14 @@
+a = 9
+b = "ali_uzaya_git"
+c = a * 2
+
+
+a / c
+a * c
+a * 5
+
+type(100)
+type(100.2)
+type(1 + 2j)
+
+a = 100.2
---
+++ substringler.py
@@ -0,0 +1,7 @@
+gel_yaz = "gelecegi_yazanlar"
+
+gel_yaz[1]
+
+gel_yaz[20]
+
+gel_yaz[0:3]
