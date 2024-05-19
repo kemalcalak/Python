02d2a83f140183154b34c7bcf8d2eb3385effe21
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-05-19 20:26:29 +0300

    feat: Add numeric validations for path parameters
    This commit adds numeric validations for the `item_id` path parameter in the `/items/{item_id}` endpoint. The `item_id` is annotated with `Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)]` to ensure that it is a positive integer between 0 and 1000. This enhancement improves the reliability and accuracy of the API by validating the input for the `item_id` path parameter.

---
+++ fast-api/tutorial/path-params-numeric-validations/main.py
@@ -0,0 +1,68 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Path, Query
+
+app = FastAPI()
+
+"""
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get")],
+    q: Annotated[str | None, Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+"""
+
+"""
+@app.get("/items/{item_id}")
+async def read_items(q: str, item_id: int = Path(title="The ID of the item to get")):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+"""
+
+"""
+@app.get("/items/{item_id}")
+async def read_items(
+    q: str, item_id: Annotated[int, Path(title="The ID of the item to get")]
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+"""
+
+"""
+@app.get("/items/{item_id}")
+async def read_items(*, item_id: int = Path(title="The ID of the item to get"), q: str):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+"""
+
+"""
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get")], q: str
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+"""
+
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+    q: str,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
