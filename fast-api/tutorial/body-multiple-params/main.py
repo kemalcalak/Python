da53051b542942c67e726df6130a354e7567fbf1
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-05-19 20:35:32 +0300

    This commit adds support for updating items with annotated body parameter in the `/items/{item_id}` endpoint. The `item` parameter is annotated with `Annotated[Item, Body(embed=True)]` to indicate that it should be embedded in the request body. This enhancement allows for more structured and validated data when updating items.

---
+++ fast-api/tutorial/body-multiple-params/main.py
@@ -0,0 +1,55 @@
+from typing import Annotated
+from fastapi import FastAPI, Path , Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+"""
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    q: str | None = None,
+    item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+"""
+
+"""
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
+"""
+
+"""
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results 
+"""
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+
